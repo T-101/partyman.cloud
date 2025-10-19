4b549d76f99d7b8ad94209430039926b4e057caa
T-101 <antti.rummukainen@gmail.com>
2025-10-19 21:57:21 +0300

    activation view

--- app/request/urls.py
+++ app/request/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from request.views import IndexView, SuccessView
+from request.views import IndexView, SuccessView, ActivationView
 
 app_name = "request"
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('success/', SuccessView.as_view(), name='success'),
+    path('activation/<int:pk>/', ActivationView.as_view(), name='activation')
 ]
