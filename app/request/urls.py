6663f4c9500dd06f53108bbbcfb9d3d50933a98b
T-101 <antti.rummukainen@gmail.com>
2025-12-06 17:15:48 +0200

    activation list and detail

--- app/request/urls.py
+++ app/request/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from request.views import LandingPageView, RequestIndexView, SuccessView, ActivationView
+from request.views import LandingPageView, RequestIndexView, SuccessView, ActivationDetailView, ActivationListView
 
 app_name = "request"
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', LandingPageView.as_view(), name='landing_page'),
     path('request/', RequestIndexView.as_view(), name='index'),
     path('success/', SuccessView.as_view(), name='success'),
-    path('activation/<int:pk>/', ActivationView.as_view(), name='activation')
+    path('activation/', ActivationListView.as_view(), name="activation-list"),
+    path('activation/<int:pk>/', ActivationDetailView.as_view(), name='activation-detail'),
 ]
