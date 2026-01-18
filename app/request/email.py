0c5eb56eb557b591dc6dfb170e9e59a91a7a616a
T-101 <antti.rummukainen@gmail.com>
2026-01-19 00:05:15 +0200

    add recipients

--- app/request/email.py
+++ app/request/email.py
@@ -3,28 +3,28 @@ import threading
 
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-from request.models import Email
+from request.models import Email, Request
 
 
-def render_emails(request, template_file: str) -> tuple[str, str]:
+def render_emails(request: Request, template_file: str) -> tuple[str, str]:
     html = render_to_string(template_file, {'request': request})
     text = re.sub(r"(.*)<style.*</style>(.*)", r"\1\2", html, count=0, flags=re.DOTALL)
     return html, strip_tags(text.strip())
 
 
-def generate_email(request, html, text):
-    recipients = [request.contact_email]
+def _generate_request_email(request: Request, html, text, recipients: list):
+    recipients = recipients if recipients else [request.contact_email]
     subject = f"Your PartyMan instance {request.party_name} {request.party_start.year}"
     customer_mail = Email(recipients=recipients, subject=subject, text_content=text, html_content=html)
 
     threading.Thread(target=customer_mail.save, daemon=True).start()
 
 
-def generate_request_received_email(request):
+def generate_request_received_email(request, recipients: list = []):
     html, text = render_emails(request, template_file="request-received.html")
-    generate_email(request, html, text)
+    _generate_request_email(request, html, text, recipients)
 
 
-def generate_request_activation_email(request):
+def generate_request_activation_email(request, recipients: list = []):
     html, text = render_emails(request, template_file="request-activated.html")
-    generate_email(request, html, text)
+    _generate_request_email(request, html, text, recipients)
