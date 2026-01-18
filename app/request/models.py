f9c2edb0419172875295008e926b046bca2ba0ed
T-101 <antti.rummukainen@gmail.com>
2026-01-18 21:35:14 +0200

    added email model. added more fields to request

--- app/request/models.py
+++ app/request/models.py
@@ -1,11 +1,17 @@
+import threading
 import re
 
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
 
 from django_extensions.db.models import TimeStampedModel
+from mailjet_rest import Client
+
+from request.email import render_emails
 
 
 def domain_validator(value):
@@ -49,9 +55,11 @@ class Testimonial(TimeStampedModel):
 class Request(TimeStampedModel):
     party_name = models.CharField(max_length=255)
     party_url = models.URLField(blank=True, null=True, help_text="URL to the party's official website")
-    contact_email = models.EmailField(help_text="Contact email for the party organizers")
+    contact_email = models.EmailField(help_text="Contact email for the party organizer(s)")
     party_start = models.DateField()
     party_end = models.DateField()
+    inception_date = models.DateField()
+    extra_info = models.TextField(blank=True, null=True)
     domain = models.CharField(max_length=255, validators=[domain_validator],
                               help_text='Subdomain for the instance without the host, usually name of the party. '
                                         'Accepted characters are a-z, 0-9 and hyphen (-). Example: myparty')
@@ -71,7 +79,6 @@ class Request(TimeStampedModel):
     deactivated = models.DateTimeField(blank=True, null=True)
     deactivated_by = models.ForeignKey(get_user_model(), blank=True, null=True, on_delete=models.SET_NULL,
                                        related_name='deactivations')
-
 
     def __str__(self):
         return f"{self.party_name} ({self.party_start.year})"
@@ -112,7 +119,7 @@ class CloudflareZone(TimeStampedModel):
         return self.name
 
 
-class SSHKeys(models.Model):
+class SSHKeys(TimeStampedModel):
     class Meta:
         verbose_name = "SSH Keys"
         verbose_name_plural = "SSH Keys"
@@ -122,3 +129,63 @@ class SSHKeys(models.Model):
 
     def __str__(self):
         return self.user
+
+
+class Email(TimeStampedModel):
+    # a post_save signal will deliver the email and populate status fields
+    recipients = models.JSONField()
+    subject = models.CharField(max_length=255)
+    text_content = models.TextField()
+    html_content = models.TextField(blank=True, null=True)
+    delivery_status_code = models.IntegerField(blank=True, null=True)
+    delivery_status_json = models.JSONField(blank=True, null=True)
+
+
+@receiver(pre_save, sender=Request)
+def prepare_emails(sender, instance, *args, **kwargs):
+    if instance.pk:
+        return
+
+    html, text = render_emails(instance, "request-received.html")
+    recipients = [instance.contact_email]
+    subject = f"Your PartyMan instance {instance.party_name}"
+    customer_mail = Email(recipients=recipients, subject=subject, text_content=text, html_content=html)
+
+    subject = f"New PartyMan request: {instance.party_name}"
+    text = f"{instance.party_name} requested by {instance.contact_email}\n\n" \
+           f"Requested inception {instance.party_start}"
+    admin_email = Email(recipients=settings.ADMINS, subject=subject, text_content=text)
+
+    for item in [customer_mail, admin_email]:
+        threading.Thread(
+            target=item.save,
+            daemon=True
+        ).start()
+
+
+@receiver(pre_save, sender=Email)
+def send_email(sender, instance, **kwargs):
+    mailjet = Client(auth=(settings.MAILJET_API_KEY, settings.MAILJET_API_SECRET), version='v3.1')
+    if instance.pk:
+        return
+
+    data = {
+        'Messages': [
+            {
+                "From": {
+                    "Email": "partyman@partyman.cloud",
+                    "Name": "PartyMan admins"
+                },
+                "To": [{"Email": x, "Name": x} for x in instance.recipients],
+                "Subject": instance.subject,
+                "TextPart": instance.text_content,
+                "HTMLPart": instance.html_content,
+                "TrackOpens": "disabled"
+            }
+        ]
+    }
+
+    res = mailjet.send.create(data=data)
+    instance.delivery_status_code = res.status_code
+    instance.delivery_status_json = res.json()
+    instance.hold_delivery = False
