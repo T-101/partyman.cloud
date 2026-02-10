dd2bb5ec2630e1d1b82ebf67125fda96fd7a6856
T-101 <antti.rummukainen@gmail.com>
2026-02-10 16:21:14 +0200

    skip steps when running in sandbox mode

--- app/request/views.py
+++ app/request/views.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 from request.forms import RequestForm, ActivationForm
 from request.helpers import get_cloudflare_dns_records, create_cloudflare_dns_entry, create_upcloud_server, \
     delete_cloudflare_dns_entry, stop_upcloud_server, delete_upcloud_server
-from request.models import Request, PortfolioItem
+from request.models import Request, PortfolioItem, AppSettings
 
 
 class LandingPageView(ListView):
@@ -69,30 +69,35 @@ class ActivationDetailView(UpdateView):
         return kwargs
 
     def form_valid(self, form):
+        settings = AppSettings.load()
         if "Deactivate" in self.request.POST["submit"]:
             request = self.get_object()
             form.instance.cloudflare_zone = request.cloudflare_zone
             form.instance.domain = request.domain
             form.instance.deactivated_by = self.request.user
-            stop_upcloud_server(form.instance)
-            delete_cloudflare_dns_entry(form.instance.cloudflare_zone, form.instance.cloudflare_dns_record_id)
-            delete_upcloud_server(form.instance)
+            form.instance.deactivated = timezone.now()
+            if not settings.sandbox_mode:
+                stop_upcloud_server(form.instance)
+                delete_cloudflare_dns_entry(form.instance.cloudflare_zone, form.instance.cloudflare_dns_record_id)
+                delete_upcloud_server(form.instance)
             form.save()
             return HttpResponseRedirect(reverse_lazy("request:activation-list"))
         else:
             # print(type(form.instance), vars(form.instance))
-            domain = form.instance.domain + "." + form.instance.cloudflare_zone.name
-            if domain in [record.name for record in get_cloudflare_dns_records(form.instance.cloudflare_zone)]:
-                form.add_error('domain', f"Domain {domain} already exists")
-                return self.form_invalid(form)
+            if not settings.sandbox_mode:
+                domain = form.instance.domain + "." + form.instance.cloudflare_zone.name
+                if domain in [record.name for record in get_cloudflare_dns_records(form.instance.cloudflare_zone)]:
+                    form.add_error('domain', f"Domain {domain} already exists")
+                    return self.form_invalid(form)
 
-            server_id, server_address = create_upcloud_server(form.instance)
-            if not server_id or not server_address:
-                form.add_error(None, "Failed to create UpCloud server")
-                return self.form_invalid(form)
-            cf_dns_id = create_cloudflare_dns_entry(form.instance.cloudflare_zone, form.instance.domain, server_address)
-            form.instance.cloudflare_dns_record_id = cf_dns_id
+                server_id, server_address = create_upcloud_server(form.instance)
+                if not server_id or not server_address:
+                    form.add_error(None, "Failed to create UpCloud server")
+                    return self.form_invalid(form)
+                cf_dns_id = create_cloudflare_dns_entry(form.instance.cloudflare_zone, form.instance.domain, server_address)
+                form.instance.cloudflare_dns_record_id = cf_dns_id
             form.instance.is_approved = True
             form.instance.activated_by = self.request.user
+            form.instance.activated = timezone.now()
 
         return super().form_valid(form)
